ab651d53b532da7b7c7db0d6fec9b84e8ba58808
SridharGeetha <ssridhar4858@gmail.com>
2025-07-25 16:01:57 +0530

    add  scs

--- Main.java
+++ Main.java
@@ -7,13 +7,8 @@ public class Main {
         int n = s1.length();
         int m = s2.length();
         
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Main");
-        }
         // int res = LongestCommonSubstring.findSubstring(s1.length(), s2.length(), s1, s2);
         // System.out.println(res);
-
-
 
         System.out.println(LongestPalindromicSubsequence.lps(n, m, s1, s2));
     }
