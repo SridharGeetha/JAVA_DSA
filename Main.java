e1cf2b565fa363076da05ef68580c82f9018ed90
SridharGeetha <ssridhar4858@gmail.com>
2025-07-25 21:55:29 +0530

    add file scs

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String s1 = "ababadc";
-        // String s2 = "xyzabdc"; 
-        String s2 = new StringBuilder(s1).reverse().toString();
+        String s1 = "brute";
+        String s2 = "groot"; 
+        // String s2 = new StringBuilder(s1).reverse().toString();
 
         int n = s1.length();
         int m = s2.length();
@@ -10,6 +10,9 @@ public class Main {
         // int res = LongestCommonSubstring.findSubstring(s1.length(), s2.length(), s1, s2);
         // System.out.println(res);
 
-        System.out.println(LongestPalindromicSubsequence.lps(n, m, s1, s2));
+        // System.out.println(LongestPalindromicSubsequence.lps(n, m, s1, s2));
+
+        String res = ShortestCommonSubsequence.scs(n, m, s1, s2);
+        System.out.println(res);
     }
 }
