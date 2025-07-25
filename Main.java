ca4682547c8258fe377872b3de624870e8ddec6a
SridharGeetha <ssridhar4858@gmail.com>
2025-07-25 11:56:29 +0530

    add for loop main

--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ public class Main {
         int n = s1.length();
         int m = s2.length();
         
-        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Main");
+        }
         // int res = LongestCommonSubstring.findSubstring(s1.length(), s2.length(), s1, s2);
         // System.out.println(res);
 
