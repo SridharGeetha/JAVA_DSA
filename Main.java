dd7a724942421c856ae1090c01c13b900fdca518
SridharGeetha <ssridhar4858@gmail.com>
2025-07-25 11:34:08 +0530

    add all files

--- Main.java
+++ Main.java
@@ -5,11 +5,14 @@ public class Main {
         String s2 = new StringBuilder(s1).reverse().toString();
 
         int n = s1.length();
+        
         int m = s2.length();
         
         
         // int res = LongestCommonSubstring.findSubstring(s1.length(), s2.length(), s1, s2);
         // System.out.println(res);
+
+
 
         System.out.println(LongestPalindromicSubsequence.lps(n, m, s1, s2));
     }
